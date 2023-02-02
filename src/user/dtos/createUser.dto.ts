566eef4db0266d2266b8252fbc2e02d75168012c
juliossena <juliosouzasena@gmail.com>
2023-02-01 23:33:28 -0300

    feat: create user (POST)

---
+++ src/user/dtos/createUser.dto.ts
@@ -0,0 +1,7 @@
+export interface CreateUserDto {
+  name: string;
+  email: string;
+  phone: string;
+  cpf: string;
+  password: string;
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
 
 @Controller('user')
 export class UserController {
-  @Get()
-  async getAllUsers() {
-    return JSON.stringify({ test: 'abc' });
+  @Post()
+  async createUser(@Body() createUser: CreateUserDto) {
+    return {
+      ...createUser,
+      password: undefined,
+    };
   }
 }
