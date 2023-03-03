28036c162d8b1ad636ed589a7a38fbbd68fa4a6d
juliossena <juliosouzasena@gmail.com>
2023-03-03 08:28:29 -0300

    feat: insert cityId in ReturnCep

--- src/correios/correios.controller.ts
+++ src/correios/correios.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { CorreiosService } from './correios.service';
-import { ReturnCepExternal } from './dto/return-cep-external.dto';
+import { ReturnCep } from './dto/return-cep.dto';
 
 @Controller('correios')
 export class CorreiosController {
   constructor(private readonly correiosService: CorreiosService) {}
 
   @Get('/:cep')
-  async findAll(@Param('cep') cep: string): Promise<ReturnCepExternal> {
+  async findAll(@Param('cep') cep: string): Promise<ReturnCep> {
     return this.correiosService.findAddressByCep(cep);
   }
 }
--- src/correios/correios.service.ts
+++ src/correios/correios.service.ts
@@ -6,7 +6,9 @@ import {
 } from '@nestjs/common';
 import { AxiosError, AxiosResponse } from 'axios';
 import { CityService } from 'src/city/city.service';
+import { CityEntity } from 'src/city/entities/city.entity';
 import { ReturnCepExternal } from './dto/return-cep-external.dto';
+import { ReturnCep } from './dto/return-cep.dto';
 
 @Injectable()
 export class CorreiosService {
@@ -16,7 +18,7 @@ export class CorreiosService {
     private readonly cityService: CityService,
   ) {}
 
-  async findAddressByCep(cep: string): Promise<ReturnCepExternal> {
+  async findAddressByCep(cep: string): Promise<ReturnCep> {
     const returnCep: ReturnCepExternal = await this.httpService.axiosRef
       .get<ReturnCepExternal>(this.URL_CORREIOS.replace('{CEP}', cep))
       .then((result) => {
@@ -31,13 +33,10 @@ export class CorreiosService {
         );
       });
 
-    const city = await this.cityService.findCityByName(
-      returnCep.localidade,
-      returnCep.uf,
-    );
+    const city: CityEntity | undefined = await this.cityService
+      .findCityByName(returnCep.localidade, returnCep.uf)
+      .catch(() => undefined);
 
-    console.log('city', city);
-
-    return returnCep;
+    return new ReturnCep(returnCep, city?.id, city?.state?.id);
   }
 }
